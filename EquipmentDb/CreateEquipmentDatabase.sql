d7760cfbe6679b5a1820cb079d593778b510f88f
GOAT-JAMES <jmm_253@yahoo.com>
2020-07-11 15:19:34 -0700

    Added database script

---
+++ EquipmentDb/CreateEquipmentDatabase.sql
@@ -0,0 +1,10 @@
+﻿CREATE DATABASE CompEquipment
+
+CREATE TABLE Equipment
+(
+	EquipId		int				PRIMARY KEY IDENTITY
+	,Name		varchar(100)	NOT NULL
+	,Price		smallmoney		NOT NULL
+)
+
+-- Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CompEquipment;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
